79e2bd5cfc379af336882843aa6f3ed12927f0a9
BogdanLi <li.bogdan44@gmail.com>
2025-05-05 16:22:14 +0500

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,28 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "React QR Code Generator",
+        short_name: "QR Code",
+        description: "QR Code Generator powered by React",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "favicon.svg",
+            sizes: "32x32",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
